729002c7d22cb7498cfa4d18cc989cbd653626ca
PuaZhiXian <xianpua2001@gmail.com>
2024-02-29 15:02:13 +0800

    1395

---
+++ src/CountNumberofTeams_1395.java
@@ -0,0 +1,43 @@
+public class CountNumberofTeams_1395 {
+    public static void main(String[] args) {
+        CountNumberofTeams_1395 obj = new CountNumberofTeams_1395();
+
+        int[] arr1 = {2, 5, 3, 4, 1};
+        int[] arr2 = {2, 1, 3};
+        int[] arr3 = {1, 2, 3, 4};
+        System.out.println(obj.numTeams(arr1));
+        System.out.println(obj.numTeams(arr2));
+        System.out.println(obj.numTeams(arr3));
+
+    }
+
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (rating[i] < rating[j]) {
+                    prefix[i]++;
+                } else {
+                    suffix[i]++;
+                }
+
+            }
+        }
+
+        int greaterCnt = 0;
+
+        int smallerCnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (rating[j] > rating[i]) {
+                    greaterCnt += prefix[j];
+                } else {
+                    smallerCnt += suffix[j];
+                }
+            }
+        }
+        return greaterCnt + smallerCnt;
+    }
+}
